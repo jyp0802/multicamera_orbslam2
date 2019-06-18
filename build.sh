afa84ad61b87e259988ca1dd78383cb9780f666a
Junyong Park <jyp0802@hotmail.com>
2019-06-18 16:37:33 +0900

    Initial commit (2), add launch file and keep yaml

--- build.sh
+++ build.sh
@@ -5,17 +5,16 @@ mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
-
-cd ../../g2o
+cd ../../..
 
 echo "Configuring and building Thirdparty/g2o ..."
 
+cd Thirdparty/g2o
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
-
-cd ../../../
+cd ../../..
 
 echo "Uncompress vocabulary ..."
 
@@ -25,8 +24,5 @@ cd ..
 
 echo "Configuring and building ORB_SLAM2 ..."
 
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
-cd ..
+cd ../../
+catkin_make .. -DCMAKE_BUILD_TYPE=Release
